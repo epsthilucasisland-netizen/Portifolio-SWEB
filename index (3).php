60556f59825838715f4d56141265ccb477151e7d
lucaschinasouza <lucas.souza.malta@gmail.com>
2026-04-13 09:29:18 -0300

    Add files via upload

---
+++ index (3).php
@@ -0,0 +1,38 @@
+<!--
+Data: 05/04/2026
+Autor:  Lucas Oliveira
+Objetivo: Criar um codigo que monte uma sequencias de numeros no estilo: 
+1
+12
+123
+
+Exercício 4 - Triângulo Numérico
+Leia um número n e imprima n linhas no seguinte formato (exemplo para n = 6):
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+-->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sequência de Números</title>
+</head>
+<body>
+
+<h1>Digite um número</h1>
+
+<form action="calcula.php" method="GET">
+    
+    Valor da quantidade de numeros: <input type="number" name="n" required>
+    <br><br>
+    
+    <input type="submit" value="Gerar">
+
+</form>
+
+</body>
+</html>
